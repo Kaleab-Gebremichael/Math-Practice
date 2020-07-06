cfa96fdf39ea3abb4d9239b0787768e8f5b1836e
Kaleab G <kaleabsolomon470@gmail.com>
2020-07-05 21:10:29 -0500

    Rough working version

--- Math Practice.py
+++ Math Practice.py
@@ -1,17 +1,36 @@
 
 from random import choice
+from random import randint
+
+DEFAULT_NUM_OF_QUESTIONS = 10
 
 QUESTION_TYPES = ['+', '-', '*', '/', '√']
-DEFAULT_NUM_OF_QUESTIONS = 10
+OPERATIONS = {
+    '+': lambda x, y: x + y,
+    '-': lambda x, y: x - y,
+    '*': lambda x, y: x * y,
+    '/': lambda x, y: x // y,
+    '√': lambda x: sqrt(x)
+}
 
 def chooseTypeOfQuestion():
     return choice(QUESTION_TYPES)
 
 def playRound(typeOfQuestion):
-    print(typeOfQuestion)
+    firstNum = randint(1,100)
+    secondNum = randint(1,100)
+    print(firstNum, " ", typeOfQuestion, " ", secondNum)
+    userAnswer = input()
+    rightAnswer = OPERATIONS[typeOfQuestion](firstNum, secondNum)
+    return userAnswer == rightAnswer
 
 
 if __name__ == '__main__':
+    score = 0
     for i in range(DEFAULT_NUM_OF_QUESTIONS):
         typeOfQuestion = chooseTypeOfQuestion()
-        playRound(typeOfQuestion)
+        correct = playRound(typeOfQuestion)
+        if (correct):
+            score += 1
+
+    print("Your final score is ", score)
